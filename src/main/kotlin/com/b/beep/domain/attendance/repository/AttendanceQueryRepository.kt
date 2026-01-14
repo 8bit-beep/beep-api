93aded37f26601ccdb32c24c3f3cf9df64b64e3e
osoohynn <tbr335@naver.com>
2026-01-14 18:12:37 +0900

    feat: change get student's currentStatus service to repository

--- src/main/kotlin/com/b/beep/domain/attendance/repository/AttendanceQueryRepository.kt
+++ src/main/kotlin/com/b/beep/domain/attendance/repository/AttendanceQueryRepository.kt
@@ -1,18 +1,17 @@
-package com.b.beep.domain.attendance.service
+package com.b.beep.domain.attendance.repository
 
 import com.b.beep.domain.attendance.domain.PeriodResolver
 import com.b.beep.domain.attendance.domain.enums.AttendanceType
-import com.b.beep.domain.attendance.repository.AttendanceRepository
 import com.b.beep.domain.user.entity.UserEntity
-import org.springframework.stereotype.Service
+import org.springframework.stereotype.Repository
 import java.time.LocalDate
 
-@Service
-class AttendanceQueryService(
+@Repository
+class AttendanceQueryRepository(
     private val attendanceRepository: AttendanceRepository,
     private val periodResolver: PeriodResolver
 ) {
-    fun getCurrentStatus(user: UserEntity): AttendanceType {
+    fun findCurrentStatus(user: UserEntity): AttendanceType {
         val currentPeriod = periodResolver.getCurrentPeriod()
         if (currentPeriod == 0) return AttendanceType.NOT_ATTEND
         return attendanceRepository
@@ -20,7 +19,7 @@ class AttendanceQueryService(
             ?.type ?: AttendanceType.NOT_ATTEND
     }
 
-    fun getCurrentStatuses(users: List<UserEntity>): Map<Long, AttendanceType> {
+    fun findAllCurrentStatuses(users: List<UserEntity>): Map<Long, AttendanceType> {
         if (users.isEmpty()) return emptyMap()
         val currentPeriod = periodResolver.getCurrentPeriod()
         if (currentPeriod == 0) return users.associate { it.id!! to AttendanceType.NOT_ATTEND }
