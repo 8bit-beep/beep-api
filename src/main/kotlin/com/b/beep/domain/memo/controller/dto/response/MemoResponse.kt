05cd637fcf3d4971d3197626f6e3d499684dd4b8
이도연 <dgsw0509@dgsw.hs.kr>
2026-02-25 00:29:16 +0900

    feat: add isRead in memo

--- src/main/kotlin/com/b/beep/domain/memo/controller/dto/response/MemoResponse.kt
+++ src/main/kotlin/com/b/beep/domain/memo/controller/dto/response/MemoResponse.kt
@@ -4,11 +4,13 @@ import com.b.beep.domain.memo.domain.entity.MemoEntity
 
 data class MemoResponse(
     val content: String,
+    val isRead: Boolean
 ) {
     companion object {
         fun of(entity: MemoEntity): MemoResponse {
             return MemoResponse(
                 content = entity.content,
+                isRead = entity.isRead
             )
         }
     }
