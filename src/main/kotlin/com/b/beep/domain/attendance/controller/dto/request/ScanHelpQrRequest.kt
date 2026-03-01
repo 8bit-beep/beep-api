706633f9842658e554ae5d7006695b879e77e35b
redjungi <redjungi09@gmail.com>
2026-03-02 02:18:20 +0900

    chore: add validation for typeId

--- src/main/kotlin/com/b/beep/domain/attendance/controller/dto/request/ScanHelpQrRequest.kt
+++ src/main/kotlin/com/b/beep/domain/attendance/controller/dto/request/ScanHelpQrRequest.kt
@@ -1,9 +1,11 @@
 package com.b.beep.domain.attendance.controller.dto.request
 
 import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Positive
 
 data class ScanHelpQrRequest(
     @field:NotBlank(message = "토큰은 필수입니다.")
     val token: String,
+    @field:Positive
     val typeId: Long
 )
