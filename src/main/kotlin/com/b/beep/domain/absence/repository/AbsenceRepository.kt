5c81736dd528b8ce3089ed6698e55a4c2a6c5a1c
이도연 <ldy2009aa@gmail.com>
2026-04-09 22:13:53 +0900

    fix: PKCE setting

--- src/main/kotlin/com/b/beep/domain/absence/repository/AbsenceRepository.kt
+++ src/main/kotlin/com/b/beep/domain/absence/repository/AbsenceRepository.kt
@@ -1,7 +1,7 @@
 package com.b.beep.domain.absence.repository
 
 import com.b.beep.domain.absence.domain.entity.AbsenceEntity
-import io.lettuce.core.dynamic.annotation.Param
+import org.springframework.data.repository.query.Param
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
