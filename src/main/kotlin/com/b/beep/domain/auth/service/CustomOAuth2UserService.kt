d1d2e676c44fd4e63e4dd797001ffb4cdbcd7251
이도연 <ldy2009aa@gmail.com>
2026-04-10 08:40:59 +0900

    fix: user-name-attribute

--- src/main/kotlin/com/b/beep/domain/auth/service/CustomOAuth2UserService.kt
+++ src/main/kotlin/com/b/beep/domain/auth/service/CustomOAuth2UserService.kt
@@ -7,11 +7,9 @@ import org.springframework.security.oauth2.client.userinfo.OAuth2UserService
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User
 import org.springframework.security.oauth2.core.user.OAuth2User
 import org.springframework.stereotype.Service
-import org.slf4j.LoggerFactory
 
 @Service
 class CustomOAuth2UserService : OAuth2UserService<OAuth2UserRequest, OAuth2User> {
-    private val log = LoggerFactory.getLogger(CustomOAuth2UserService::class.java)
     override fun loadUser(userRequest: OAuth2UserRequest): OAuth2User {
         val delegate = DefaultOAuth2UserService()
         val oauth2User = delegate.loadUser(userRequest)
@@ -20,7 +18,6 @@ class CustomOAuth2UserService : OAuth2UserService<OAuth2UserRequest, OAuth2User>
             .providerDetails.userInfoEndpoint.userNameAttributeName
 
         val attributes = oauth2User.attributes
-        log.info("DAuth attributes: $attributes")
 
         return DefaultOAuth2User(
             setOf(SimpleGrantedAuthority("ROLE_USER")),
